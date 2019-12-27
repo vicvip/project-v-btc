e9791c5d7960e8c90916bab21bf71da64cef65d0
vicvip <victor_gtavt@yahoo.com>
2019-12-27 06:54:22 -0800

    implemented interconnected hall with corridor

--- Assets/Scripts/TilesGenerator.cs
+++ Assets/Scripts/TilesGenerator.cs
@@ -16,31 +16,31 @@ public class TilesGenerator : MonoBehaviour
 
     private List <Vector3> gridPositions = new List<Vector3>();
 
-    void initialiseList() 
-    {
-        
-    }
-
     // Start is called before the first frame update
     void Start()
     {
         boardHolder = new GameObject("Board").transform;
 
         instantiateHall(rows, columns, 0, 0);
-
         var selectEdgeTiles = objectPositionList.Where(o => o.IsEdge == true).ToList();
         var chosenTile = selectEdgeTiles[Random.Range(0, selectEdgeTiles.Count)];
-        
-        instantiateCorridor(chosenTile);
+        var corridorEnd = instantiateCorridor(chosenTile);
 
+        instantiateHall(rows, columns, corridorEnd.X, corridorEnd.Z);
+        
         Debug.Log($"Counter is {chosenTile.Counter}, X is {chosenTile.X}, Z is {chosenTile.Z}");
         //instantiateCorridor()
     }
 
     private void instantiateHall(int columns, int rows, int xInitial, int zInitial) {
-        for (int x = xInitial; x < columns + xInitial; x++) {
-            for(int z = zInitial; z < rows + zInitial; z++) {
-                GameObject instance = Instantiate(floorTile, new Vector3(x * 1.1F, 0, z * 1.1F), Quaternion.identity) as GameObject;
+        var xStandardised = xInitial < 0 ? -1F : 1F;
+        var zStandardised = zInitial < 0 ? -1F : 1F;
+        int xInitStandardised = xInitial * (int)xStandardised;
+        int zInitStandardised = zInitial * (int)zStandardised;
+
+        for (int x = xInitStandardised; x < columns + xInitStandardised; x++) {
+            for(int z = zInitStandardised; z < rows + zInitStandardised;  z++) {
+                GameObject instance = Instantiate(floorTile, new Vector3(x * 1.1F * xStandardised, 0, z * 1.1F * zStandardised), Quaternion.identity) as GameObject;
 
                 var newPosition = new ObjectPosition
                 {
@@ -50,10 +50,6 @@ public class TilesGenerator : MonoBehaviour
                     ObjectInstance = instance,
                     IsEdge = false
                 };
-
-                // if(x == xInitial || x == columns - 1 || z == zInitial || z == rows - 1) {
-                //     newPosition.IsEdge = true;
-                // }
 
                 if(x == xInitial) {
                     newPosition.IsEdge = true;
@@ -75,24 +71,17 @@ public class TilesGenerator : MonoBehaviour
                     newPosition.EdgeStance = 0;
                 }
 
-
-                // if(x == 0 && z == 0) {
-                //     instantiateCorridor(-1, 0);
-                // }
-
                 objectPositionList.Add(newPosition);
                 instance.transform.SetParent(boardHolder);
-                
-                //Debug.Log($"x is {x}, z is {z}");
                 counter++;
 
             }
         }
     }
 
-    //private void instantiateCorridor(int row, int column) {
-    private void instantiateCorridor(ObjectPosition objectPosition) {
-        var corridorSize = Random.Range(1, 5);
+    private ObjectPosition instantiateCorridor(ObjectPosition objectPosition) {
+        GameObject instanceCorridor = null;
+        var corridorSize = Random.Range(2, 5);
         float xVector = objectPosition.X;
         float zVector = objectPosition.Z;
 
@@ -112,9 +101,23 @@ public class TilesGenerator : MonoBehaviour
                     break;
             }
 
-            GameObject instanceCorridor = Instantiate(floorTile, new Vector3(xVector * 1.1F, 0, zVector * 1.1F), Quaternion.identity) as GameObject;
+            instanceCorridor = Instantiate(floorTile, new Vector3(xVector * 1.1F, 0, zVector * 1.1F), Quaternion.identity) as GameObject;
             instanceCorridor.transform.SetParent(boardHolder);
         }
+
+        var corridorEnd = new ObjectPosition
+        {
+            X = (int)xVector,
+            Z = (int)zVector,
+            Counter = counter,
+            ObjectInstance = instanceCorridor,
+            IsEdge = true
+        };
+        Debug.Log($"Corridor: x is {xVector}, z is {zVector}");
+        
+        counter++;
+
+        return corridorEnd;
     }
 
     // Update is called once per frame
