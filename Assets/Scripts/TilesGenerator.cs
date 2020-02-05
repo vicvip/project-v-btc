60446537ded624bc81a787c9975b0a1193e556ff
vicvip <victor_gtavt@yahoo.com>
2020-02-05 21:53:16 +1100

    added assets, walls and character

--- Assets/Scripts/TilesGenerator.cs
+++ Assets/Scripts/TilesGenerator.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
@@ -8,10 +9,14 @@ public class TilesGenerator : MonoBehaviour
 {
     public Transform boardHolder;
     public Transform boardHolder2;
-    public GameObject floorTile;
+    public GameObject floorTiles;
+    public GameObject wallTiles;
+    public GameObject[] wallTiless;
     public int columns = 8;
     public int rows = 8;
+    public float fm = 1.1f;
     int counter = 0;
+    
     private List<ObjectPosition> objectPositionList = new List<ObjectPosition>();
 
     private List <Vector3> gridPositions = new List<Vector3>();
@@ -22,13 +27,14 @@ public class TilesGenerator : MonoBehaviour
         boardHolder = new GameObject("Board").transform;
 
         instantiateHall(rows, columns, 0, 0);
-        var selectEdgeTiles = objectPositionList.Where(o => o.IsEdge == true).ToList();
+        var selectEdgeTiles = objectPositionList.Where(o => o.IsEdge).ToList();
         var chosenTile = selectEdgeTiles[Random.Range(0, selectEdgeTiles.Count)];
         var corridorEnd = instantiateCorridor(chosenTile);
 
         instantiateHall(rows, columns, corridorEnd.X, corridorEnd.Z);
-        
-        Debug.Log($"Counter is {chosenTile.Counter}, X is {chosenTile.X}, Z is {chosenTile.Z}");
+        SpawnWalls(selectEdgeTiles, chosenTile);
+        //LayoutObjectAtRandom(wallTiles, 5, 9);
+        //Debug.Log($"Counter is {chosenTile.Counter}, X is {chosenTile.X}, Z is {chosenTile.Z}");
         //instantiateCorridor()
     }
 
@@ -40,7 +46,7 @@ public class TilesGenerator : MonoBehaviour
 
         for (int x = xInitStandardised; x < columns + xInitStandardised; x++) {
             for(int z = zInitStandardised; z < rows + zInitStandardised;  z++) {
-                GameObject instance = Instantiate(floorTile, new Vector3(x * 1.1F * xStandardised, 0, z * 1.1F * zStandardised), Quaternion.identity) as GameObject;
+                GameObject instance = Instantiate(floorTiles, new Vector3(x * fm * xStandardised, 0, z * fm * zStandardised), Quaternion.identity) as GameObject;
 
                 var newPosition = new ObjectPosition
                 {
@@ -101,7 +107,7 @@ public class TilesGenerator : MonoBehaviour
                     break;
             }
 
-            instanceCorridor = Instantiate(floorTile, new Vector3(xVector * 1.1F, 0, zVector * 1.1F), Quaternion.identity) as GameObject;
+            instanceCorridor = Instantiate(floorTiles, new Vector3(xVector * fm, 0, zVector * fm), Quaternion.identity) as GameObject;
             instanceCorridor.transform.SetParent(boardHolder);
         }
 
@@ -118,6 +124,65 @@ public class TilesGenerator : MonoBehaviour
         counter++;
 
         return corridorEnd;
+    }
+
+    private void SpawnWalls(List<ObjectPosition> tiles, ObjectPosition dontSpawntile = null)
+    {
+        var zMax = tiles.Select(t => t.Z).Max() + 1;
+        var xMin = tiles.Select(t => t.X).Min();
+        var xMax = tiles.Select(t => t.X).Max();
+
+        foreach(var tile in tiles)
+        {   
+            //Debug.Log($"xTile is {tile.X} = {dontSpawntile.X}, zTile is {tile.Z} = {dontSpawntile.Z}");
+            if(tile.EdgeStance == 2 || (tile.X == dontSpawntile.X && tile.Z == dontSpawntile.Z)) {
+                continue;
+            }
+            var xTile = tile.X;
+            var zTile = tile.Z;
+            switch(tile.EdgeStance)
+            {
+                case 0:
+                    zTile = zTile + 1;
+                    break;
+                case 1:
+                    xTile = xTile + 1;
+                    break;
+                case 3:
+                    xTile = xTile - 1;
+                    break;
+            }
+            GameObject instance1 = Instantiate(SelectRandomObjects(wallTiless), new Vector3(xTile * fm, 0, zTile * fm), Quaternion.identity) as GameObject;
+        }
+
+        GameObject instance2 = Instantiate(SelectRandomObjects(wallTiless), new Vector3(xMin * fm, 0, zMax * fm), Quaternion.identity) as GameObject;
+        GameObject instance3 = Instantiate(SelectRandomObjects(wallTiless), new Vector3(xMax * fm, 0, zMax * fm), Quaternion.identity) as GameObject;
+    }
+
+    Vector3 RandomPosition()
+    {
+        var randomIndex = Random.Range(0, gridPositions.Count);
+        var randomPosition = gridPositions[randomIndex];
+        gridPositions.RemoveAt(randomIndex);
+        return randomPosition;
+    }
+
+    GameObject SelectRandomObjects(GameObject[] gameObjects)
+    {
+        var randomIndex = Random.Range(0, gameObjects.Length);
+        var randomPosition = gameObjects[randomIndex];
+        return randomPosition;
+    }
+
+    void LayoutObjectAtRandom(GameObject[] tileArray, int min, int max)
+    {
+        var objectCount = Random.Range(min, max + 1);
+        for(int i = 0; i < objectCount; i++)
+        {
+            var randomPosition = RandomPosition();
+            var tileChoice = tileArray[Random.Range(0, tileArray.Length)];
+            Instantiate(tileChoice, randomPosition, Quaternion.identity);
+        }
     }
 
     // Update is called once per frame
